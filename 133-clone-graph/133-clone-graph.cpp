5f82f8ec1f6b35d794bfbd1e0e37779318e5cba3
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-01-03 19:28:33 +0530

    Time: 19 ms (7.23%), Space: 8.9 MB (51.17%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    unordered_map<Node*,Node*> mp;
+    void  dfs(Node* node){
+        Node* copy = new Node(node->val);
+        mp[node] = copy;
+        for(auto neigh: node->neighbors){
+            if(mp.find(neigh) == mp.end()){
+                dfs(neigh);
+            }
+            copy->neighbors.push_back(mp[neigh]);
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        if(!node) {
+            return node;
+        }
+        dfs(node);
+        return mp[node];
+    }
+};
