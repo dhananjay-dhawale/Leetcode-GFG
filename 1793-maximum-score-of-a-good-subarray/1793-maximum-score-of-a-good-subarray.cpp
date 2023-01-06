9e0f9cbc5dccde5b9a86b11ad52155354ec5fd6e
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-01-06 20:08:06 +0530

    Time: 442 ms (39.59%), Space: 89.6 MB (60.76%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector <int> left_mx(n), right_mx(n);
-        
-        stack <int> s;
-        for(int i = 0; i < n; i++) {
-            while(!s.empty() && nums[s.top()] >= nums[i]) {
-                s.pop();
+        int mn = nums[k], i = k, j = k, res = nums[k];
+        do {
+            if (i > 0 && j < nums.size() - 1) {
+              if (nums[i - 1] >= nums[j + 1])
+                    --i;
+                else
+                    ++j;
             }
-            if(s.empty()) {
-                left_mx[i] = 0;
-            }
-            else {
-                left_mx[i] = s.top() + 1;
-            }
-            s.push(i);
+            else if (i == 0 && j < nums.size() - 1)
+                ++j;
+            else if (j == nums.size() - 1 && i > 0)
+                --i;
+            mn = min({mn, nums[i], nums[j]});
+            res = max(res, mn * (j - i + 1));            
         }
-        
-        while(!s.empty()) {
-            s.pop();
-        }
-        
-        for(int i = n - 1; i >= 0; i--) {
-            while(!s.empty() && nums[s.top()] >= nums[i]) {
-                s.pop();
-            }
-            if(s.empty()) {
-                right_mx[i] = n - 1;
-            }
-            else {
-                right_mx[i] = s.top() - 1;
-            }
-            s.push(i);
-        }
-        
-        int ans = 0;
-        for(int idx = 0; idx < n; idx++) {
-            int i  = left_mx[idx];
-            int j  = right_mx[idx];
-            int mn = nums[idx];
-            if(i <= k && k <= j) {
-                ans = max(ans, mn * (j - i + 1));
-            }
-        }
-        cout << "\n";
-        return ans;
+        while (i > 0 || j < nums.size() - 1); 
+        return res;
     }
 };
