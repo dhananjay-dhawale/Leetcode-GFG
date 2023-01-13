82bd37a2f548da3d313e420c9b7b2381fdb0ee7b
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-01-13 13:57:04 +0530

    Time: 411 ms (88.26%), Space: 181.2 MB (65.77%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -3,35 +3,24 @@ private:
     int ans ;
     string s;    
 public:
-    void dfs(int node, vector <vector<int>> &cnt, vector <int> adj[]) {
-        int mx1 = 0, mx2 = 0;
-        for(auto &child : adj[node]) {
-            cnt[s[child] - 'a'][child] += 1;
-            dfs(child, cnt, adj);
-            if(s[node] != s[child]) {
-                int c = cnt[s[child] - 'a'][child];
-                if(c >= mx1) {
-                    mx2 = mx1;
-                    mx1 = c;
-                }
-                else if(c >= mx2) {
-                    mx2 = c;
-                }
-            }
-        }
-        cnt[s[node] - 'a'][node] = 1 + mx1;
-        ans = max(ans, 1 + mx1 + mx2);
+     int longestPath(vector<int>& parent, string s) {
+        int n = s.size(), res = 0;
+        vector<vector<int>> children(n, vector<int>());
+        for (int i = 1; i < n; ++i)
+            children[parent[i]].push_back(i);
+        dfs(children, s, res, 0);
+        return res;
     }
-    int longestPath(vector<int>& parent, string s) {
-        this->s = s;
-        int n = s.size();
-        vector <int> adj[n];
-        for(int i = 1; i < n; i++) {
-            adj[parent[i]].push_back(i);
+
+    int dfs(vector<vector<int>>& children, string& s, int& res, int i) {
+        int big1 = 0, big2 = 0;
+        for (int& j : children[i]) {
+            int cur = dfs(children, s, res, j);
+            if (s[i] == s[j]) continue;
+            if (cur > big2) big2 = cur;
+            if (big2 > big1) swap(big1, big2);
         }
-        vector <vector<int>> cnt(26, vector <int> (n, 0));
-        ans = 0;
-        dfs(0, cnt, adj);
-        return ans;
+        res = max(res, big1 + big2 + 1);
+        return big1 + 1;
     }
 };
