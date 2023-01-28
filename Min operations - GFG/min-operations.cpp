b8ca3ebf56963cd487e773af55b67f3e3dcd2999
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-01-29 00:12:17 +0530

    Added solution - LeetHub

---
+++ Min operations - GFG/min-operations.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int a, int b) {
+        return a == b ? 0 : (((a&b) == a) || ((a&b) == b)) ? 1 : 2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.solve(a, b);
+        
+        printf("%d\n", res);
+        
+    }
+}
+
+// } Driver Code Ends
